3bf76f8573674efa9257a0c60c2ffd011c24f3e3
Shri Om Trivedi <tshriom@gmail.com>
2023-02-26 00:53:54 +0530

    serializers added, urls.py added for roomView

---
+++ collaborative_music_system/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Room
+
+# Serializers allow complex data such as querysets and model instances to be converted to native Python datatypes that can then be easily rendered into JSON, XML or other content types. Serializers also provide deserialization, allowing parsed data to be converted back into complex types, after first validating the incoming data.
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause',
+                  'votes_to_skip', 'created_at')
--- collaborative_music_system/api/urls.py
+++ collaborative_music_system/api/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import RoomView
 
 # If we get URL that is blank and nothing have on it the call the main function /views.
 urlpatterns = [
-    path('home', main),
-    path('', main)
+    path('home', RoomView.as_view()),
 ]
