6d343c9e104e29a0eded1f426318d3a0d45ffcf9
Shri Om Trivedi <tshriom@gmail.com>
2023-02-28 21:29:43 +0530

    tailwind init,

--- collaborative_music_system/collaborative_music_system/urls.py
+++ collaborative_music_system/collaborative_music_system/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', include('frontend.urls')) # When path is not admin/ or api/ we will send request to frontend.urls
 ]
