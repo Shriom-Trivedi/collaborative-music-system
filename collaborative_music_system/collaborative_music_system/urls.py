1e0cda12fd39f40748fc80250021f9efbac40475
Shri Om Trivedi <tshriom@gmail.com>
2023-02-28 08:11:13 +0530

    patch changes

--- collaborative_music_system/collaborative_music_system/urls.py
+++ collaborative_music_system/collaborative_music_system/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', include('frontend.urls')) # When path is not admin/ or api/ we will send request to frontend.urls
 ]
